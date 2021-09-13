038719601e011ab87bab1c84886aff9bac2ec3ee
Mateusz Gałuszka <galuszka1996@gmail.com>
2021-09-13 20:14:41 +0200

    add views

---
+++ my-app/src/components/views/HomePage/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './HomePage.module.scss';
+
+const HomePage = () => {
+  return (
+    <div className={styles.component}>
+      <h2>
+        Home view
+      </h2>
+      
+    </div>
+  );
+};
+
+export default HomePage;
